47f2ecb6b5654904cc1dd22fb2554a87b8472b86
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-07-06 17:24:22 -0500

    Added gameplay methods

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -8,9 +8,9 @@ public class VirtualPet {
     private int tiredness;
     private int mood;
 
-    // Constructor 
+    // Constructor
 
-    public VirtualPet(){
+    public VirtualPet() {
     }
 
     // Getter and Setters
@@ -63,4 +63,43 @@ public class VirtualPet {
     }
 
     // Methods
+    public void tick() {
+        setHunger(getHunger() - 5);
+        setThirst(getThirst() - 5);
+        setTiredness(getTiredness() - 5);
+        if (getHunger() >= 100 || getThirst() >= 100) {
+            setMood(getMood() - 0);
+        } else if (getHunger() >= 70 || getThirst() >= 70) {
+            setMood(getMood() - 5);
+        } else if (getHunger() >= 40 || getThirst() >= 40) {
+            setMood(getMood() - 10);
+        } else if (getHunger() >= 20 || getThirst() >= 20) {
+            setMood(getMood() - 20);
+        }
+    }
+
+    public void feed() {
+        System.out.println("You gave " + getName() + " their favorite food! They seemed to really enjoy it...");
+        System.out.println("Hunger Level Increased by 20.");
+        setHunger(getHunger() + 20);
+    }
+
+    public void drink() {
+        System.out.println("You gave " + getName() + " a glass of water. They really chugged it down...");
+        System.out.println("Thirst Level Increased by 20.");
+        setThirst(getThirst() + 20);
+    }
+
+    public void Play() {
+        System.out.println("You and " + getName() + " played a fun game of virtual volleyball. \033[3m You let them win...\033[0m");
+        System.out.println("Mood Increased by 30.");
+        setMood(getMood() + 30);
+    }
+
+    public void Sleep() {
+        System.out.println("You tucked " + getName() + " into their virtual bed, they really got comfortable...");
+        System.out.println(getName() + " became well rested.");
+        setTiredness(100);
+    }
+
 }
